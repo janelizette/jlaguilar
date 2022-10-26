cc138beeefa4a1de34887ea35526e39346081b3c
Cyanide-zero <doteiy001@gmail.com>
2022-10-26 21:14:01 +0800

    FullpageJS implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import "./App.css";
+import ReactFullpage from "@fullpage/react-fullpage"
 import Form from "./form/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
-}
+const anchors = ["Hi!", "About"]; // Dagdag ka isa dito pag dadagdag ka section, eto yung names sa gilid
+
+const App = () => (
+  <ReactFullpage
+    // loopBottom={true}
+    // loopTop={true}
+    anchors={anchors}
+    scrollingSpeed={1000}
+    navigation
+    navigationTooltips={anchors}
+    sectionsColor={["#0c0c0c","#009182"]} // Dagdag ka isa dito pag dadagdag ka section, background colors to
+    onLeave={(origin, destination, direction) => {
+      console.log("onLeave event", { origin, destination, direction });
+    }}
+    render={({ state, fullpageApi }) => {
+      console.log("render prop change", state, fullpageApi);
+      return (
+        <div>
+          <div className="section"><Form/></div>
+          <div className="section" style={{height:'100vh', width:'100vw'}}><p>Section</p></div>
+        </div>
+      );
+    }}
+  />
+);
 
 export default App;
