fa49a3e1d2a182dc69cb5de11fe8b77d71d95f93
janelizette <janelizetteaguilar@gmail.com>
2022-10-31 21:26:16 +0800

    Add contact form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ReactFullpage from "@fullpage/react-fullpage";
 
 import Introduction from "./components/Introduction";
 import Education from "./components/Education";
-import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
 
-const anchors = ["Hi!", "About"]; // Dagdag ka isa dito pag dadagdag ka section, eto yung names sa gilid
+const anchors = ["Hi!", "About", "Contact"]; // Dagdag ka isa dito pag dadagdag ka section, eto yung names sa gilid
 
 const App = () => (
   <ReactFullpage
@@ -15,19 +15,22 @@ const App = () => (
     scrollingSpeed={1000}
     navigation
     navigationTooltips={anchors}
-    sectionsColor={["#202020", "#202020"]} // Dagdag ka isa dito pag dadagdag ka section, background colors to
+    sectionsColor={["#202020", "#202020", "#202020"]} // Dagdag ka isa dito pag dadagdag ka section, background colors to
     onLeave={(origin, destination, direction) => {
-      console.log("onLeave event", { origin, destination, direction });
+      // console.log("onLeave event", { origin, destination, direction });
     }}
     render={({ state, fullpageApi }) => {
-      console.log("render prop change", state, fullpageApi);
+      // console.log("render prop change", state, fullpageApi);
       return (
         <div>
-          <div className="section" style={{width: "100vw", height: "100vh"}}>
+          <div className="section" style={{ width: "100vw", height: "100vh" }}>
             <Introduction />
           </div>
-          <div className="section" style={{width: "100vw", height: "100vh"}}>
+          <div className="section" style={{ width: "100vw", height: "100vh" }}>
             <Education />
+          </div>
+          <div className="section" style={{ width: "100vw", height: "100vh" }}>
+            <ContactMe />
           </div>
         </div>
       );
---
+++ src/components/ContactMe.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import validator from "validator";
+import Swal from "sweetalert2";
+import Contact from "../styles/ContactMe.module.css";
+
+function ContactMe() {
+  const [showForm, setShowForm] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (validator.isEmail(email)) {
+      emailjs
+        .sendForm(
+          "service_bvsvmj5",
+          "template_31evzds",
+          form.current,
+          "soQMU5-SICIbkYbi9"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            console.log("email sent");
+            Swal.fire({
+              icon: "success",
+              title: "Hooray!",
+              text: "Email sent successfully.",
+            });
+            document.getElementById("contact-form").reset();
+          },
+          (error) => {
+            console.log(error.text);
+            Swal.fire({
+              icon: "error",
+              title: "Oops!",
+              text: "Sorry, something went wrong there. Try again.",
+            });
+          }
+        );
+    } else {
+      console.log("error");
+    }
+  };
+
+  return (
+    <div className={Contact.container}>
+      <div className={Contact.title}>
+        <h1 className={Contact.heading1}>Get in touch</h1>
+      </div>
+
+      {/* <div className={Contact.content}>
+        <p className={Contact.paragraph}>
+          Send me a message and I'll get back to you as soon as possible!
+        </p>
+        <button
+          className={Contact.button}
+          onClick={() => {
+            setShowForm(true);
+          }}
+        >
+          Say hello
+        </button>
+        <p className={Contact.paragraph}>
+          Or download my resume for your reference!
+        </p>
+
+        <a
+          href="https://drive.google.com/file/d/1YdW8TPrgiIcE_Yd7qKTrAYWizPkNrhl4/view?usp=share_link"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={Contact.link}
+        >
+          Resume
+        </a>
+      </div> */}
+
+      <div className={Contact.content}>
+        <p className={Contact.paragraph}>
+          Send me a message and I'll get back to you as soon as possible!
+        </p>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className={Contact.formContainer}
+          id="contact-form"
+        >
+          <div className={Contact.nameEmailContainer}>
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Name"
+              className={[Contact.name, Contact.inputs].join(" ")}
+              required
+            />
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Email"
+              className={`${Contact.email} ${Contact.inputs}`}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <textarea
+            name="message"
+            placeholder="Message"
+            className={`${Contact.message} ${Contact.inputs}`}
+            required
+          />
+          <input type="submit" value="Send" className={Contact.button} />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactMe;
